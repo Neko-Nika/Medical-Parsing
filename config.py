c14a97d61e6dc7e363e15a366e91b0d1a243049c
Neko-Nika <78418054+Neko-Nika@users.noreply.github.com>
2022-05-30 19:29:08 +0300

    Add config file

---
+++ config.py
@@ -0,0 +1,13 @@
+DEFAULT_SAMPLES = 10
+MIN_SAMPLES = 1
+MAX_SAMPLES = 100
+MAX_CLINICS = 20
+DEFAULT_CLINICS_FROM = 3
+DEFAULT_CLINICS_TO = 5
+
+sources = {
+    'МОСГОРМЕД': 'https://mosgormed.ru/#vrachi',
+    #'СТОП-ЛИСТ.ИНФО': 'http://stop-list.info/baza/poisk_vracha_po_vsej_rossii_baza_dannykh_vrachej_s_foto/2020-02-12-1569'
+}
+
+output_formats = ['xls', 'xlsx', 'csv']

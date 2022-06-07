63f6e1ebdb6a65b8cdc7e80dc422327d2c90fda1
Neko-Nika <78418054+Neko-Nika@users.noreply.github.com>
2022-06-07 21:20:16 +0300

    Update config

--- config.py
+++ config.py
@@ -4,6 +4,9 @@ MAX_SAMPLES = 100
 MAX_CLINICS = 20
 DEFAULT_CLINICS_FROM = 3
 DEFAULT_CLINICS_TO = 5
+DOCTORS_PER_PAGE = 15
+MIN_DAYWORK_TIME = 5
+MAX_DAYWORK_TIME = 8
 
 sources = {
     'МОСГОРМЕД': 'https://mosgormed.ru/#vrachi',
@@ -11,3 +14,14 @@ sources = {
 }
 
 output_formats = ['xls', 'xlsx', 'csv']
+columns = [
+    'id[pk]',
+    'Имя', 'Фамилия', 'Отчество', 'Должность',
+    'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс',
+    'Номер телефона', 'Email', 'Клиники',
+    'Номер медицинской лицензии', 'Срок действия', 'Стаж'
+]
+russian_number_codes = [
+    '910', '915', '916', '917', '919', '920', '985', '986', '995', '996', '999'
+]
+email_source = 'https://lavrynenko.com/spisok-poddelnyx-email-adresov-generator/'
